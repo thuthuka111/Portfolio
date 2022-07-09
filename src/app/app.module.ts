9510dc2c32eed1faec493cd969bfa50dcb2947a8
thuthuka111 <thuthuka111@gmail.com>
2022-07-09 15:03:41 +0200

    category component shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule
