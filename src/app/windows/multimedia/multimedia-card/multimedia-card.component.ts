7cbe40c7fac9b856209846517c9ac1b9698f8151
thuthuka111 <thuthuka111@gmail.com>
2023-01-17 18:29:39 +0200

    half implemented cards

--- src/app/windows/multimedia/multimedia-card/multimedia-card.component.ts
+++ src/app/windows/multimedia/multimedia-card/multimedia-card.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'multimedia-card',
   templateUrl: './multimedia-card.component.html',
-  styleUrls: ['./multimedia-card.component.css']
+  styleUrls: ['./multimedia-card.component.sass']
 })
 export class MultimediaCardComponent {
 
--- src/app/windows/window-card/window-card.component.ts
+++ src/app/windows/window-card/window-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface Media {
+  mediaUrl: string,
+  description?: string,
+  image: boolean,
+}
 
 @Component({
   selector: 'general-card',
   templateUrl: './window-card.component.html',
-  styleUrls: ['./window-card.component.css']
+  styleUrls: ['./window-card.component.sass']
 })
 export class WindowCardComponent {
-
+  @Input() text: string;
+  @Input() media: Media[];
+  @Input() reverse: boolean | undefined;
 }
