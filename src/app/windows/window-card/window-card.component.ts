a2102545e94b27d378a1a97aff0ea5d6a843e262
thuthuka111 <thuthuka111@gmail.com>
2023-02-01 12:23:21 +0200

    card width and height variable

--- src/app/windows/window-card/window-card.component.ts
+++ src/app/windows/window-card/window-card.component.ts
@@ -13,18 +13,38 @@ interface Media {
 })
 export class WindowCardComponent implements AfterViewInit {
   @ViewChild('media_platform') mediaPlatformEl: ElementRef<HTMLElement> | undefined;
-  @ViewChild('text_platform') textPlatformEl:  ElementRef<HTMLElement> | undefined;
-  
+  @ViewChild('text_platform') textPlatformEl: ElementRef<HTMLElement> | undefined;
+
   @ViewChild('media_shadow') mediaShadowEl: ElementRef<HTMLElement> | undefined;
-  @ViewChild('text_shadow') textShadowEl:  ElementRef<HTMLElement> | undefined;
+  @ViewChild('text_shadow') textShadowEl: ElementRef<HTMLElement> | undefined;
+
+  @ViewChild('media_container') mediaContainerEl: ElementRef<HTMLElement> | undefined;
 
   @Input() colour: string | undefined;
   @Input() shadowColour: string | undefined;
   @Input() text: string;
   @Input() media: Media[];
   @Input() reverse: boolean | string | undefined;
+  @Input() widthDif: number | undefined;
+  @Input() heightDif: number | undefined;
 
   ngAfterViewInit() {
+    if (this.mediaContainerEl.nativeElement) {
+      if (this.widthDif) {
+        let baseWidth = 600;
+
+        baseWidth += this.widthDif;
+        this.mediaContainerEl.nativeElement.style.width = `${baseWidth}px`;
+      }
+
+      if (this.heightDif) {
+        let baseHeight = 500;
+
+        baseHeight += this.heightDif;
+        this.mediaContainerEl.nativeElement.style.height = `${baseHeight}px`;
+      }
+    }
+
     if (this.mediaPlatformEl.nativeElement && this.textPlatformEl.nativeElement && this.colour) {
       this.textPlatformEl.nativeElement.style.backgroundColor = this.colour;
       this.mediaPlatformEl.nativeElement.style.backgroundColor = this.colour;
