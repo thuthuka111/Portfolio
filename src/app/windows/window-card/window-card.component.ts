50429b112a9d1204ee0e3edb8bd36369cecea5c7
thuthuka111 <thuthuka111@gmail.com>
2023-02-01 11:58:22 +0200

    general card colour editable

--- src/app/windows/window-card/window-card.component.ts
+++ src/app/windows/window-card/window-card.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 interface Media {
-  mediaUrl: string,
+  mediaURL: string,
   description?: string,
   image: boolean,
 }
@@ -11,8 +11,28 @@ interface Media {
   templateUrl: './window-card.component.html',
   styleUrls: ['./window-card.component.sass']
 })
-export class WindowCardComponent {
+export class WindowCardComponent implements AfterViewInit {
+  @ViewChild('media_platform') mediaPlatformEl: ElementRef<HTMLElement> | undefined;
+  @ViewChild('text_platform') textPlatformEl:  ElementRef<HTMLElement> | undefined;
+  
+  @ViewChild('media_shadow') mediaShadowEl: ElementRef<HTMLElement> | undefined;
+  @ViewChild('text_shadow') textShadowEl:  ElementRef<HTMLElement> | undefined;
+
+  @Input() colour: string | undefined;
+  @Input() shadowColour: string | undefined;
   @Input() text: string;
   @Input() media: Media[];
-  @Input() reverse: boolean | undefined;
+  @Input() reverse: boolean | string | undefined;
+
+  ngAfterViewInit() {
+    if (this.mediaPlatformEl.nativeElement && this.textPlatformEl.nativeElement && this.colour) {
+      this.textPlatformEl.nativeElement.style.backgroundColor = this.colour;
+      this.mediaPlatformEl.nativeElement.style.backgroundColor = this.colour;
+    }
+
+    if (this.mediaShadowEl.nativeElement && this.textShadowEl.nativeElement && this.shadowColour) {
+      this.mediaShadowEl.nativeElement.style.backgroundColor = this.shadowColour;
+      this.textShadowEl.nativeElement.style.backgroundColor = this.shadowColour;
+    }
+  }
 }
